e393b7f1fa50dda2c7e607c4669e06ba99705234
Sikriet <matthgamez@gmail.com>
2022-05-19 16:37:19 -0400

    - New Activity for the list of products. - TO-DO: Create the class "RecyclerViewAdapter" and get the products list from a data base.

--- app/src/main/java/com/example/fixit/MainActivity.java
+++ app/src/main/java/com/example/fixit/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.fixit;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -15,6 +17,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +32,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .add(R.id.map, mapFragment)
                 .commit();
         mapFragment.getMapAsync(this);
+
+        recyclerView = findViewById(R.id.rvMechanicsList);
+
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        //mAdapter = new RecyclerViewAdapter(productsList); *
+        // TO-DO: Create the class "RecyclerViewAdapter" and get the products list.
+        recyclerView.setAdapter(mAdapter);
     }
 
     @SuppressLint("MissingPermission")
